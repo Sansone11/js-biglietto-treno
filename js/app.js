2ce902a002465e2e694b8ba4f5e87d303c3aa13b
Sansone11 <sansonemirco@gmail.com>
2022-07-20 17:27:30 +0200

    result

--- js/app.js
+++ js/app.js
@@ -2,13 +2,24 @@ console.log('JS ok');
 // chiedi all'utente il numero di chilometri che desidera percorrere
 // chiedi l'età del passeggiero
 // moltiplica il numero di chilometri per il prezzo al chilometro
-// se età è inferiore a 18 moltiplica  per 0.2
-// se età e superiore a 65 moltiplica per 0.4
+// se età è inferiore a 18 sottrai il prezzo e moltiplica per 0.2 
+// se età e superiore a 65 sottrai il prezzo e moltiplica per 0.4
 const kilometriUtente =parseInt( prompt ('numero di chilometri'));
-console.log ("kilometri: ", kilometriUtente)
+
 const etaUtente =parseInt( prompt ('quanti anni hai?'));
-console.log ("eta utente: ",etaUtente)
+
 const prezzoKilometro = 0.21;
+
 const discountOver = 0.4;
+
 const discountUnder = 0.2;
-const result = (kilometriUtente * prezzoKilometro);
+
+let result = kilometriUtente * prezzoKilometro;
+
+if (etaUtente < 18){
+    result = result - (result * 0.2);
+}else if(etaUtente > 65){
+    result = result - (result * 0.4);
+}
+document.getElementById(price).innerHTML + result;
+
