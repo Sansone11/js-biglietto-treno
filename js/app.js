968ad18ca34913d4eb0a341752659b05d33df246
Sansone11 <sansonemirco@gmail.com>
2022-07-20 16:04:02 +0200

    calcolo-prezzo

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@ console.log('JS ok');
 // moltiplica il numero di chilometri per il prezzo al chilometro
 // se età è inferiore a 18 moltiplica  per 0.2
 // se età e superiore a 65 moltiplica per 0.4
-const numeroKilometri = prompt ('numero di chilometri');
-const età = prompt ('età');
+const kilometriUtente =parseInt( prompt ('numero di chilometri'));
+console.log ("kilometri: ", kilometriUtente)
+const etaUtente =parseInt( prompt ('quanti anni hai?'));
+console.log ("eta utente: ",etaUtente)
 const prezzoKilometro = 0.21;
 const discountOver = 0.4;
 const discountUnder = 0.2;
-const result = numerokilometri * prezzoKilometro;
-console.log (result)
+const result = (numerokilometri * prezzoKilometro);
