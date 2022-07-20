2b1940bbae5af50284f805aff82818c041f8514b
Sansone11 <sansonemirco@gmail.com>
2022-07-20 15:48:07 +0200

    variabili

---
+++ js/app.js
@@ -0,0 +1,13 @@
+console.log('JS ok');
+// chiedi all'utente il numero di chilometri che desidera percorrere
+// chiedi l'età del passeggiero
+// moltiplica il numero di chilometri per il prezzo al chilometro
+// se età è inferiore a 18 moltiplica  per 0.2
+// se età e superiore a 65 moltiplica per 0.4
+const numeroKilometri = prompt ('numero di chilometri');
+const età = prompt ('età');
+const prezzoKilometro = 0.21;
+const discountOver = 0.4;
+const discountUnder = 0.2;
+const result = numerokilometri * prezzoKilometro;
+console.log (result)
